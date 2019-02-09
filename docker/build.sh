66eed3eec0a30c857fadd9f1c6779f08840ca686
Stefan Bueringer <sbueringer@gmail.com>
2019-02-09 09:12:58 +0100

    fixed docker build

--- docker/build.sh
+++ docker/build.sh
@@ -3,15 +3,15 @@
 WORKDIR=`echo $0 | sed -e s/build.sh//`
 cd ${WORKDIR}/../
 
+IMAGE=${1:-"docker.io/sbueringer/alertmanager-silence-exporter:v0.1.0"}
+
 rm -rf docker/dist
 CGO_ENABLED=0 go build -a -installsuffix cgo -v -o docker/dist/alertmanager-silence-exporter cmd/alertmanager-silence-exporter/main.go
 
-docker build -t reg-dhc.app.corpintra.net/caas/alertmanager-silence-exporter:v0.1.0 docker/
-
-docker push reg-dhc.app.corpintra.net/caas/alertmanager-silence-exporter:v0.1.0
+docker build -t  ${IMAGE} docker/
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z $DOCKER_PASSWORD ]
 then
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-    docker push docker.io/sbueringer/squid:latest;
+    docker push ${IMAGE};
 fi
